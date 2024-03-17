51d24f6592e6c40d714712d7269da6b59cf0225f
kumar4454 <150816543+kumar4454@users.noreply.github.com>
2024-03-17 22:39:28 +0530

    Update ai_clone.py

--- ai_clone.py
+++ ai_clone.py
@@ -11,16 +11,16 @@ d="<div style='border:solid black 2px'>hello</div>"
 st.markdown(d, unsafe_allow_html=True)
 
 #prompt
-h=[]
+#h=[]
 p=st.text_input(label="", placeholder="enter a prompt here.")
 clicked=st.button("ask....")
 c=st.button("history")
 if clicked:
-    h.append(p)
-    st.write(h)
+    #h.append(p)
+    #st.write(h)
     st.write(f"prompt you entered:  {p}")
     response= model.generate_content([p])
     st.write(response.text)
     st.write("thank you...!!")
-if c:
-    st.write(f"history: {h}")
+#if c:
+    #st.write(f"history: {h}")
