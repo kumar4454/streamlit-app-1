7410b188fa5a5bbb583756ab29e1406e4312c4ab
kumar4454 <150816543+kumar4454@users.noreply.github.com>
2024-03-17 22:38:15 +0530

    Create ai_clone.py

---
+++ ai_clone.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyDokwegR04F-D94Olo8l8QcgVpd6MCJ0QU")
+model= genai.GenerativeModel("gemini-pro")
+
+#title
+
+st.markdown("<h1 style='text-align:center'>Gemini Ai Clone.</h1>", unsafe_allow_html=True) 
+
+d="<div style='border:solid black 2px'>hello</div>"
+st.markdown(d, unsafe_allow_html=True)
+
+#prompt
+h=[]
+p=st.text_input(label="", placeholder="enter a prompt here.")
+clicked=st.button("ask....")
+c=st.button("history")
+if clicked:
+    h.append(p)
+    st.write(h)
+    st.write(f"prompt you entered:  {p}")
+    response= model.generate_content([p])
+    st.write(response.text)
+    st.write("thank you...!!")
+if c:
+    st.write(f"history: {h}")
