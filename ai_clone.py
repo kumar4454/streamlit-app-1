7fe7b2e440f329f5522c4992c753f63aa54b6a9a
kumar4454 <150816543+kumar4454@users.noreply.github.com>
2024-03-18 06:30:57 +0530

    Update ai_clone.py

--- ai_clone.py
+++ ai_clone.py
@@ -7,14 +7,14 @@ model= genai.GenerativeModel("gemini-pro")
 
 st.markdown("<h1 style='text-align:center'>Gemini Ai Clone.</h1>", unsafe_allow_html=True) 
 
-d="<div style='border:solid black 2px'>hello</div>"
-st.markdown(d, unsafe_allow_html=True)
+#d="<div style='border:solid black 2px'>hello</div>"
+#st.markdown(d, unsafe_allow_html=True)
 
 #prompt
 #h=[]
 p=st.text_input(label="", placeholder="enter a prompt here.")
 clicked=st.button("ask....")
-c=st.button("history")
+#c=st.button("history")
 if clicked:
     #h.append(p)
     #st.write(h)
