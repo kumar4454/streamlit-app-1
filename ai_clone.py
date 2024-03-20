baf18cd639b98f4c165a001c069eb1ac3fe0aa5c
kumar4454 <150816543+kumar4454@users.noreply.github.com>
2024-03-20 21:37:44 +0530

    Update ai_clone.py

--- ai_clone.py
+++ ai_clone.py
@@ -2,17 +2,17 @@ import streamlit as st
 import google.generativeai as genai
 from streamlit_option_menu import option_menu
 
-genai.configure(api_key= "AIzaSyDokwegR04F-D94Olo8l8QcgVpd6MCJ0QU")
+api=st.secrets["genai_api_key"]
+
+genai.configure(api_key= api)
 model= genai.GenerativeModel("gemini-pro")
 
 #title
 
 st.markdown("<h1 style='text-align:center'>Gemini Ai Clone.</h1>", unsafe_allow_html=True) 
 
-with st.sidebar:(
-    st.button("New Chat"),
-    st.write("clicked")
-)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 selected=option_menu(
     menu_title=None,
@@ -22,18 +22,32 @@ selected=option_menu(
     orientation="horizontal",
 )
 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 #prompt
-p=st.chat_input("enter your prompt")
+prompt = st.chat_input("enter your prompt")
 
-if p is None:
-    # Handle the case where p is None
-    p = ""
+if prompt is None:
+    with st.chat_message("assistant"):
+        st.markdown("hello, how can i help you")
+#else:
+#    print("Error: Input data 'p' is None.")
 
-if p:
+
+if prompt is not None:
     with st.chat_message("user"):
-        st.markdown(p)
+        st.markdown(prompt)
+    #response= model.generate_content([prompt])
+    st.session_state.messages.append({"role":"user", "content":prompt})
+#else:
+ #   default_prompt = "hello"
+ #   response = model.generate_content([default_prompt])
 
-response= model.generate_content([p])
+response= model.generate_content([prompt])
+
 if response:
     with st.chat_message("assistant"):
         st.markdown(response.text)
+    st.session_state.messages.append({"role":"assistant", "content":response.text})
