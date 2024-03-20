3f9a51e647b9b73a2323d8d42706fe726173ef9a
kumar4454 <150816543+kumar4454@users.noreply.github.com>
2024-03-20 07:12:30 +0530

    Update ai_clone.py

--- ai_clone.py
+++ ai_clone.py
@@ -1,26 +1,41 @@
 import streamlit as st
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyDokwegR04F-D94Olo8l8QcgVpd6MCJ0QU")
+from streamlit_option_menu import option_menu
+
+api=st.secrets["genai_api_key"]
+
+genai.configure(api_key= api)
 model= genai.GenerativeModel("gemini-pro")
 
 #title
 
 st.markdown("<h1 style='text-align:center'>Gemini Ai Clone.</h1>", unsafe_allow_html=True) 
 
-#d="<div style='border:solid black 2px'>hello</div>"
-#st.markdown(d, unsafe_allow_html=True)
+with st.sidebar:(
+    st.button("New Chat"),
+    st.write("clicked")
+)
+
+selected=option_menu(
+    menu_title=None,
+    menu_icon=None,
+    options=["home","settings","about us"],
+    icons=["house","gear wheel","book"],
+    orientation="horizontal",
+)
 
 #prompt
-#h=[]
-p=st.text_input(label="", placeholder="enter a prompt here.")
-clicked=st.button("ask....")
-#c=st.button("history")
-if clicked:
-    #h.append(p)
-    #st.write(h)
-    st.write(f"prompt you entered:  {p}")
-    response= model.generate_content([p])
-    st.write(response.text)
-    st.write("thank you...!!")
-#if c:
-    #st.write(f"history: {h}")
+p=st.chat_input("enter your prompt")
+
+if p is None:
+    # Handle the case where p is None
+    p = ""
+
+if p:
+    with st.chat_message("user"):
+        st.markdown(p)
+
+response= model.generate_content([p])
+if response:
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
